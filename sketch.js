d4ae19e287bc24dcf2761853c446a33e3c159ebe
TheREALBoazLee <boazlee071218@gmail.com>
2023-01-31 21:17:20 -0500

    Testing
    testing

---
+++ sketch.js
@@ -0,0 +1,18 @@
+let image;
+
+function loadout(){
+  image = loadimage("wargame.png");
+
+}
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(250,250,250);
+  image(200,200);
+
+}
